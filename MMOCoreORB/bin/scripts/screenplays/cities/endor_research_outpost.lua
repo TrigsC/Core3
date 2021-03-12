6d0822e34f8b81ca2bf4416a5b4c97d9018e9cb1
Hakry <Hakry@swgemu.com>
2021-03-12 00:06:52 -0500

    [changed] Randomized Endor and Lok City filler NPCs
    Change-Id: I58accdf8147bd5d15f9a1c94a887aa23eabcb281

--- MMOCoreORB/bin/scripts/screenplays/cities/endor_research_outpost.lua
+++ MMOCoreORB/bin/scripts/screenplays/cities/endor_research_outpost.lua
@@ -18,6 +18,25 @@ EndorResearchOutpostScreenPlay = CityScreenPlay:new {
 		r3_1 = {{3251, 24, -3466, 0, 1}, {3267, 24, -3450, 0, 1}, {3269, 24, -3431, 0, 1}, {3278, 24, -3442, 0, 1}, {3273, 24, -3443, 0, 1}},
 		r5_1 = {{3250, 24, -3467, 0, 1}, {3249, 24, -3493, 0, 1}, {3244, 24, -3497, 0, 1}, {3227, 24, -3493, 0, 1}, {3222, 24, -3493, 0, 1}, {3204, 24, -3489, 0, 1}, {3208, 24, -3480, 0, 1}},
 	},
+
+	stationaryCommoners = {"commoner", "commoner_fat", "commoner_old"},
+	stationaryNpcs = {"bodyguard", "bounty_hunter", "businessman", "commoner_technician", "contractor", "entertainer", "explorer", "fringer", "gambler", "medic", "mercenary", "miner", "noble", "pilot", "rancher", "scientist", "slicer"},
+
+	--{respawn, x, z, y, direction, cell, mood}
+	stationaryMobiles = {
+		{1, 3175.37, 24, -3490.78, 156.98, 0, ""},
+		{1, 3190.35, 24, -3434.93, 256.64, 0, ""},
+		{1, 3239.41, 24, -3432.75, 321.916, 0, ""},
+		{1, 3270.48, 24, -3452.45, 289.69, 0, ""},
+		{1, 3246.59, 24, -3500.57, 28.4927, 0, ""},
+		{1, 3209.65, 24, -3493.64, 0, 0, ""},
+		{1, 3272.55, 24, -3438.99, 185.321, 0, ""},
+		{1, 3255.57, 24, -3483.66, 195.741, 0, ""},
+		{1, 3271.4, 24, -3502.14, 322.22, 0, ""},
+		{1, 3201.34, 24, -3509.29, 348.067, 0, ""},
+		{1, 3221.86, 24, -3473.87, 251.839, 0, ""},
+		{1, 3228.48, 24, -3510.48, 15.5858, 0, ""},
+	},
 }
 
 registerScreenPlay("EndorResearchOutpostScreenPlay", true)
@@ -26,6 +45,7 @@ function EndorResearchOutpostScreenPlay:start()
 	if (isZoneEnabled("endor")) then
 		self:spawnMobiles()
 		self:spawnPatrolMobiles()
+		self:spawnStationaryMobiles()
 	end
 end
 
@@ -34,29 +54,4 @@ function EndorResearchOutpostScreenPlay:spawnMobiles()
 	--tavern building
 	pNpc = spawnMobile("endor", "kilnstrider",60,-3.44448,0.624999,-6.82681,331.362,9925367)
 	self:setMoodString(pNpc, "npc_imperial")
-
-	--outside
-	spawnMobile("endor", "businessman", 60, 3175.37, 24, -3490.78, 156.98, 0)
-	spawnMobile("endor", "businessman", 60, 3190.35, 24, -3434.93, 256.64, 0)
-	spawnMobile("endor", "businessman", 60, 3239.41, 24, -3432.75, 321.916, 0)
-	spawnMobile("endor", "businessman", 60, 3270.48, 24, -3452.45, 289.69, 0)
-	spawnMobile("endor", "businessman", 60, 3246.59, 24, -3500.57, 28.4927, 0)
-	spawnMobile("endor", "businessman", 60, 3209.65, 24, -3493.64, 217.211, 0)
-
-	spawnMobile("endor", "commando", 300, 3196.77, 24, -3483.16, 359.892, 0)
-	spawnMobile("endor", "commando", 300, 3166, 24, -3454, 130, 0)
-	spawnMobile("endor", "commoner", 60, 3176.73, 24, -3512.02, 187.645, 0)
-	spawnMobile("endor", "commoner", 60, 3188.73, 24, -3482.54, 277.835, 0)
-	spawnMobile("endor", "commoner", 60, 3186.49, 24, -3459.94, 189.74, 0)
-	spawnMobile("endor", "commoner", 60, 3171.1, 24,- 3504.6, 196.748, 0)
-	spawnMobile("endor", "commoner", 60, 3272.55, 24, -3438.99, 185.321, 0)
-	spawnMobile("endor", "commoner", 60, 3255.57, 24, -3483.66, 195.741, 0)
-	spawnMobile("endor", "commoner", 60, 3271.4, 24, -3502.14, 322.22, 0)
-	spawnMobile("endor", "commoner", 60, 3201.34, 24, -3509.29, 348.067, 0)
-	spawnMobile("endor", "commoner", 60, 3221.86, 24, -3473.87, 251.839, 0)
-	spawnMobile("endor", "mercenary", 60, 3277, 24, -3484, -80, 0)
-	spawnMobile("endor", "mercenary", 60, 3233, 24, -3451, 170, 0)
-	spawnMobile("endor", "mercenary", 60, 3201, 24, -3463, 170, 0)
-	spawnMobile("endor", "noble", 60, 3228.48, 24, -3510.48, 15.5858, 0)
-
 end
--- MMOCoreORB/bin/scripts/screenplays/cities/endor_smuggler_outpost.lua
+++ MMOCoreORB/bin/scripts/screenplays/cities/endor_smuggler_outpost.lua
@@ -14,6 +14,21 @@ EndorSmugglerOutpostScreenPlay = CityScreenPlay:new {
 		--table_name = {{x, z, y, cell, delayAtNextPoint}} 1 = no delay 0 = delay}
 		r3_1 = {{-951, 73, 1542, 0, 1}, {-905, 76, 1586, 0, 1}, {-904, 80, 1612, 0, 1}, {-886, 76, 1600, 0, 1}, {-880, 76, 1589, 0, 1}, {-858, 76, 1584, 0, 1}, {-854, 79, 1600, 0, 1}, {-858, 76, 1584, 0, 1}, {-885, 76, 1586, 0, 1}},
 	},
+
+	stationaryCommoners = {"commoner", "commoner_fat", "commoner_old"},
+	stationaryNpcs = {"bodyguard", "bounty_hunter", "businessman", "commoner_technician", "contractor", "entertainer", "explorer", "fringer", "gambler", "medic", "mercenary", "miner", "noble", "pilot", "rancher", "scientist", "slicer"},
+
+	--{respawn, x, z, y, direction, cell, mood}
+	stationaryMobiles = {
+		{1, -899.977, 78, 1548.09, 107.809, 0, ""},
+		{1, -828.009, 76, 1573.28, 320.402, 0, ""},
+		{1, -864.293, 79.5001, 1598.78, 184.686, 0, ""},
+		{1, -875.462, 80.0597, 1571.89, 84.1865, 0, ""},
+		{1, -956, 73, 1554, 135, 0, ""},
+		{1, -874.656, 80, 1564.76, 270, 0, ""},
+		{1, -845.301, 79.5, 1599.49, 197.393, 0, ""},
+		{1, -829.243, 76, 1567.61, 95.886, 0, ""},
+	},
 }
 
 registerScreenPlay("EndorSmugglerOutpostScreenPlay", true)
@@ -22,11 +37,11 @@ function EndorSmugglerOutpostScreenPlay:start()
 	if (isZoneEnabled("endor")) then
 		self:spawnMobiles()
 		self:spawnPatrolMobiles()
+		self:spawnStationaryMobiles()
 	end
 end
 
 function EndorSmugglerOutpostScreenPlay:spawnMobiles()
-
 	--mission term building
 	local pNpc = spawnMobile("endor", "commoner_technician",60,3.5,0.1,3.9,0,3605972)
 	self:setMoodString(pNpc, "sad")
@@ -34,23 +49,4 @@ function EndorSmugglerOutpostScreenPlay:spawnMobiles()
 	--tavern
 	local pNpc = spawnMobile("endor", "commoner_old",60,1.0,0.7,-4.4,0,6645605)
 	self:setMoodString(pNpc, "npc_sitting_chair")
-
-	--outside
-	spawnMobile("endor", "businessman", 60, -899.977, 78, 1548.09, 107.809, 0)
-	spawnMobile("endor", "commoner", 60, -828.009, 76, 1573.28, 320.402, 0)
-	spawnMobile("endor", "commoner", 60, -838.625, 76, 1574.97, 333.27, 0)
-	spawnMobile("endor", "commoner", 60, -864.293, 79.5001, 1598.78, 184.686, 0)
-	spawnMobile("endor", "commoner", 60, -841.483, 79.5144, 1602.34, 240.991, 0)
-	spawnMobile("endor", "commoner", 60, -851.058, 80.0739, 1555.56, 238.812, 0)
-	spawnMobile("endor", "commoner", 60, -875.462, 80.0597, 1571.89, 84.1865, 0)
-	spawnMobile("endor", "commoner", 60, -880.276, 76, 1604.61, 30.4167, 0)
-	spawnMobile("endor", "commoner", 60, -880.194, 76, 1624.44, 273.171, 0)
-	spawnMobile("endor", "commoner", 60, -880.718, 76, 1638.25, 11.1628, 0)
-	spawnMobile("endor", "commoner", 60, -896.026, 80, 1621.19, 243.324, 0)
-	spawnMobile("endor", "scientist", 60, -874.656, 80, 1564.76, 108.353, 0)
-	spawnMobile("endor", "scientist", 60, -845.301, 79.5, 1599.49, 197.393, 0)
-	spawnMobile("endor", "smuggler", 300, -889.786, 76, 1591.32, 182.753, 0)
-	spawnMobile("endor", "smuggler", 300, -890.91, 76, 1591.38, 253.162, 0)
-	spawnMobile("endor", "smuggler", 300, -889.732, 76, 1592.45, 319.914, 0)
-	spawnMobile("endor", "noble", 60, -829.243, 76, 1567.61, 95.886, 0)
 end
