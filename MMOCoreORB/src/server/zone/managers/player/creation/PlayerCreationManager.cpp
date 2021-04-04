6d2b76e883292e1f81964dec3ed072acdb8c5a58
Victor Popovici <victorpopovici@gmail.com>
2021-04-04 21:15:09 +0200

    [fixed] coverity warnings
    Change-Id: If492e33fea0def25f73ad8d986a3081abe030a76

--- MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
+++ MMOCoreORB/src/server/zone/managers/player/creation/PlayerCreationManager.cpp
@@ -150,6 +150,12 @@ void PlayerCreationManager::loadProfessionDefaultsInfo() {
 	iffStream = templateManager->openIffFile(
 			"datatables/creation/profession_mods.iff");
 
+	if (iffStream == nullptr) {
+		error("Could not open creation profession mods data table");
+
+		return;
+	}
+
 	DataTableIff dtiff;
 	dtiff.readObject(iffStream);
 
